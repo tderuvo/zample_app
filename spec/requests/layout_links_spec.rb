12a9eb86a57e16af20bc2ef4a3757473ccc8fdeb
tony deruvo <tonyderuvo@mac.com>
2011-09-14 16:34:50 -0400

    Working index page

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -78,11 +78,13 @@ describe "LayoutLinks" do
                                          :content => "Profile")
     end
     
-    it "should have a settings link" do
+    it "should have a users link" do
       visit root_path
-      response.should have_selector("a", :href => edit_user_path(@user),
-                                         :content => "Settings")
-    end
+      response.should have_selector("a", :href => users_path,
+                                         :content => "Users")
+    end 
+
+
   end
    
 end
